3f64212b3a116aeb493c20fc39edc2837eadf1ca
dora5n <Moe4dora5n@gmail.com>
2023-06-18 16:29:52 +0900

    js修正

--- js/app.js
+++ js/app.js
@@ -4,109 +4,125 @@
 // $("").on("click", function () {});
 
 class TodoList {
-  // コンストラクタ
-  constructor() {
-          this.DOM = {};
-          this.DOM.incomplete = document.querySelector(".item-area-incomplete ul");
-          this.DOM.complete = document.querySelector(".item-area-complete ul");
+    constructor() {
+        this.DOM = {};
+        this.DOM.incomplete = document.querySelector(".item-area-incomplete ul");
+        this.DOM.complete = document.querySelector(".item-area-complete ul");
+    }
 
-      }
-      // リストを生成
-  _createItem(text) {
-          const liElm = document.createElement('li');
-          const pElm = document.createElement('p');
-          const deleteButton = document.createElement('button');
-          const completeButton = document.createElement('button');
+    _createItem(text) {
+        const liElm = document.createElement("li");
+        const pElm = document.createElement("p");
+        const deleteButton = document.createElement("button");
+        const completeButton = document.createElement("button");
 
-          completeButton.classList.add('complete-button');
-          completeButton.innerText = '完了';
-          deleteButton.classList.add('incomplete-button');
-          deleteButton.innerText = '削除';
-          pElm.innerText = text;
+        completeButton.classList.add("complete-button");
+        completeButton.innerText = "完了";
+        deleteButton.classList.add("incomplete-button");
+        deleteButton.innerText = "削除";
+        pElm.innerText = text;
 
-          // 完了ボタンクリック
-          completeButton.addEventListener('click', (e) => {
-              this.deleteItem(e.srcElement.parentNode, this.DOM.incomplete);
-              this.addCompleteItem(text);
-          });
-          // 削除ボタンクリック
-          deleteButton.addEventListener('click', (e) => {
-              const confirmation = confirm("タスクを削除しても良いですか？");
+        completeButton.addEventListener("click", () => {
+        this.deleteItem(liElm, this.DOM.incomplete);
+        this.addCompleteItem(text);
+        this.saveToLocalStorage();
+    });
 
-              if (confirmation) {
-                  this.deleteItem(e.srcElement.parentNode, this.DOM.incomplete);
-              }
-          });
+    deleteButton.addEventListener("click", () => {
+        const confirmation = confirm("タスクを削除しても良いですか？");
 
-          // 生成した要素
-          liElm.appendChild(pElm);
-          liElm.appendChild(completeButton);
-          liElm.appendChild(deleteButton);
+        if (confirmation) {
+            this.deleteItem(liElm, this.DOM.incomplete);
+            this.saveToLocalStorage();
+        }
+    });
+    
+    liElm.appendChild(pElm);
+    liElm.appendChild(completeButton);
+    liElm.appendChild(deleteButton);
+    
+    return liElm;
+    }
 
-          return liElm;
-      }
-      // 完了に追加
-  _completeItem(text) {
-          const liElm = document.createElement('li');
-          const pElm = document.createElement('p');
-          const backButton = document.createElement('button');
+    _completeItem(text) {
+        const liElm = document.createElement("li");
+        const pElm = document.createElement("p");
+        const backButton = document.createElement("button");
 
-          backButton.classList.add('complete-button');
-          backButton.innerText = '戻る';
-          pElm.innerText = text;
-          // 戻るボタンクリック
-          backButton.addEventListener('click', (e) => {
-              this.deleteItem(e.srcElement.parentNode, this.DOM.complete);
-              this.addItem(text);
-          });
+        backButton.classList.add("complete-button");
+        backButton.innerText = "戻る";
+        pElm.innerText = text;
 
-          liElm.appendChild(pElm);
-          liElm.appendChild(backButton);
+        backButton.addEventListener("click", () => {
+        this.deleteItem(liElm, this.DOM.complete);
+        this.addItem(text);
+        this.saveToLocalStorage();
+    });
 
-          return liElm;
-      }
-      // リストを追加
-  addItem(text) {
-          this.DOM.incomplete.appendChild(this._createItem(text));
-      }
-      // 完了に追加
-  addCompleteItem(text) {
-          this.DOM.complete.appendChild(this._completeItem(text));
-      }
-      // リストを削除
-  deleteItem(target, domparent) {
-      domparent.removeChild(target);
-  }
-}
+    liElm.appendChild(pElm);
+    liElm.appendChild(backButton);
 
-function addtodoEvent() {
-  const addItemTxt = document.querySelector(".add-item").value;
-  // 値を入力していない時は処理を終了
-  if (addItemTxt == "") {
-      alert("値を入力してください");
-      return
-  }
-  // フォームの値をリセット
-  document.querySelector(".add-item").value = "";
-  // インスタンス化
-  const totoList = new TodoList();
-  totoList.addItem(addItemTxt);
-}
+    return liElm;
+    }
 
-// クリックイベント
-document.querySelector(".add-button").addEventListener('click', () => {
-  addtodoEvent();
-});
+    addItem(text) {
+        this.DOM.incomplete.appendChild(this._createItem(text));
+        this.saveToLocalStorage();
+    }
 
-// キーボードイベント
-document.addEventListener('keypress', keypress);
-const regexEnter = new RegExp('(=|Enter)');
+    addCompleteItem(text) {
+        this.DOM.complete.appendChild(this._completeItem(text));
+        this.saveToLocalStorage();
+    }
 
-function keypress(e) {
-  // エンターが押されたら
-  if (regexEnter.test(e.key)) {
-      addtodoEvent();
-  } else {
-      return false;
-  }
-}
+    deleteItem(target, domparent) {
+        domparent.removeChild(target);
+        this.saveToLocalStorage();
+    }
+
+    saveToLocalStorage() {
+        const incompleteItems = Array.from(this.DOM.incomplete.children).map(
+        (item) => item.querySelector("p").innerText
+        );
+        const completeItems = Array.from(this.DOM.complete.children).map(
+        (item) => item.querySelector("p").innerText
+        );
+
+    localStorage.setItem(
+        "todoList",
+        JSON.stringify({
+            incomplete: incompleteItems,
+            complete: completeItems,
+        })
+    );
+    }
+
+    loadFromLocalStorage() {
+        const todoListData = localStorage.getItem("todoList");
+        
+        if (todoListData) {
+        const { incomplete, complete } = JSON.parse(todoListData);
+
+        incomplete.forEach((item) => {
+            this.addItem(item);
+        });
+        
+        complete.forEach((item) => {
+            this.addCompleteItem(item);
+        });
+    }
+    }
+    }
+    
+    function addTodoEvent() {
+        const addItemTxt = document.querySelector(".add-item").value;
+        if (addItemTxt == "") {
+            alert("値を入力してください");
+            return;}
+            document.querySelector(".add-item").value = "";
+            const totoList = new TodoList();
+            totoList.addItem(addItemTxt);
+        }
+        
+        document.querySelector(".add-button").addEventListener("click", addTodoEvent);
+        document.addEventListener
